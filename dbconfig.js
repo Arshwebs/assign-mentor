dd446cb8b2c70f53c3caeaec27e9c0442107ac3b
Arsh700 <arshathm4@gmail.com>
2023-03-10 16:27:58 +0530

    first commit

---
+++ dbconfig.js
@@ -0,0 +1,4 @@
+const mongodb = require("mongodb");
+const dbName = "classdb";
+const dbUrl = `mongodb+srv://arsh:welcome123@cluster0.ye06dga.mongodb.net/${dbName}`;
+module.exports = {mongodb, dbName, dbUrl};
